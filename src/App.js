1dfb20b284d4e3b7fc8c4f7e45fed028582553b2
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2022-10-06 16:46:50 +0300

    connected app to redux store

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import { Provider } from 'react-redux';
 import Navbar from './components/Navbar';
 import Books from './components/Books';
 import Categories from './components/Categories';
+import store from './redux/configureStore';
 
 function App() {
   return (
-   <Provider store={store}>
-    <div className="books-store">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Books />} />
-        <Route path="/catagories" element={<Categories />} />
-      </Routes>
-    </div>
-   </Provider>
+    <Provider store={store}>
+      <div className="books-store">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/catagories" element={<Categories />} />
+        </Routes>
+      </div>
+    </Provider>
   );
 }
 

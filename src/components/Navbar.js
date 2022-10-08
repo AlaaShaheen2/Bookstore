b34ea420dedecc50de0ab709504d46fb42c94a99
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2022-10-09 00:32:12 +0300

    navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,32 @@
 import './styles/navbar.css';
 import { Link } from 'react-router-dom';
-import img from './logoin.jpeg';
+import user from './user.png';
 
 const Navbar = () => (
 
-  <nav className="navbar">
-    <span className="nav-logo">Bookstore CMS</span>
-    <ul className="nav-list">
-      <li>
-        <Link to="/" className="nav-item books">
-          {' '}
-          Books
-        </Link>
-      </li>
-      <li>
-        <Link to="/catagories" className="nav-item categories">
-          Categories
-        </Link>
-      </li>
-    </ul>
-  </nav>
+  <header className="header">
+    <nav className="navbar">
+      <span className="nav-logo">Bookstore CMS</span>
+      <ul className="nav-list">
+        <li>
+          <Link to="/" className="nav-item books">
+            {' '}
+            Books
+          </Link>
+        </li>
+        <li>
+          <Link to="/catagories" className="nav-item categories">
+            Categories
+          </Link>
+        </li>
+      </ul>
+    </nav>
+    <span>
+      <Link to="/" className="nav-profile">
+        <img src={user} alt="hi" className="user-img" />
+      </Link>
+    </span>
+  </header>
 
 );
 
